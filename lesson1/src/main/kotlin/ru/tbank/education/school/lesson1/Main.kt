28fe9bb4437b765c13685858aa0719024756d474
Slava <viacheslavkalinichev@gamil.com>
2025-11-06 10:49:07 +0300

    We started cooking

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,6 +1,15 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    print("Hi")
-    println("tBank!")
+    println("Hi")
+    print("tBank!")
+    print("\n")
+    val d = 123     // константа
+    val a: Int? = 8
+    var ans: Boolean? = null
+    val g = "hell $d world"
+    var h = 1       // переменная
+    if (a != null) {
+        println(a)
+    }
 }
