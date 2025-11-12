233147fe6b04b4356306619cf12f0a9cab296c1a
Slava <viacheslavkalinichev@gamil.com>
2025-11-12 22:01:30 +0300

    Sum of even numbers

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -1,8 +1,16 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Сумма четных чисел.
- */
+// Сумма четных чисел
 fun sumEvenNumbers(numbers: Array<Int>): Int {
-    TODO()
+    var sum = 0
+    for (i in numbers) {
+        if (i % 2 == 0) {
+            sum += i
+        }
+    }
+    return sum
+}
+
+fun filter(numbers: Array<Int>): Int {
+    return numbers.filter{it % 2 == 0}.sum()
 }
--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,15 +1,21 @@
 package ru.tbank.education.school.lesson1
+import kotlin.system.exitProcess
 
 fun main() {
     println("Hi")
     print("tBank!")
     print("\n")
-    val d = 123     // константа
+    val d = 'o'     // константа
     val a: Int? = 8
     var ans: Boolean? = null
-    val g = "hell $d world"
+    val g = "hell$d world"
+    println(g)
     var h = 1       // переменная
     if (a != null) {
         println(a)
     }
+    for (i in 1..3 step 2) {
+        println(i)
+    }
+    exitProcess(0)
 }
